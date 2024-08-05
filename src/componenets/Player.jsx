7bc23f60b5e5eba52c7fab91504ad275392fa850
JCDiaz1201 <jcdiaz1201git@gmail.com>
2024-08-05 16:20:46 -0500

    Work done on player component

---
+++ src/componenets/Player.jsx
@@ -0,0 +1,32 @@
+import {useState} from "react";
+
+export default function Player({initialName, symbol}) {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing((editing) => !editing)
+    }
+
+    // 'event' object is auto made available to this function from a pointer
+    function handleChange(event) {
+        setPlayerName(event.target.value)
+    }
+
+    let playerNameElement = <span className={"player-name"}>{playerName}</span>
+
+    if (isEditing) {
+        // Below is two-way binding
+        playerNameElement = <input type={"text"} required value={playerName} onChange={handleChange} />
+    }
+
+    return (
+        <li>
+          <span className={"player"}>
+            {playerNameElement}
+            <span className={"player-symbol"}>{symbol}</span>
+          </span>
+            <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+        </li>
+    );
+}
