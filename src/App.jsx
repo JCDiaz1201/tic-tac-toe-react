30367ce44843cf6c379c2104927f8a9672ca6295
JCDiaz1201 <jcdiaz1201git@gmail.com>
2024-08-06 16:22:52 -0500

    Pushing up updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import {useState} from "react";
+import GameBoard from "./componenets/gameboard/GameBoard.jsx";
 import Player from "./componenets/Player.jsx";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelectSquare() {
+    setActivePlayer((currentlyActivePlayer) =>
+        currentlyActivePlayer === 'X' ? 'O' : 'X'
+    )
+  }
 
   return <main>
     <div id={"game-container"}>
-      <ol id={"players"}>
-        <Player initialName={"player 1"} symbol={"X"} />
-        <Player initialName={"player 2"} symbol={"O"} />
+      <ol id={"players"} className={"highlight-player"}>
+        <Player initialName={"player 1"} symbol={"X"} isActive={activePlayer === 'X'} />
+        <Player initialName={"player 2"} symbol={"O"} isActive={activePlayer === 'O'} />
       </ol>
-
+      <GameBoard onSelectSquare={handleSelectSquare} activePlayerSym={activePlayer}/>
     </div>
   </main>
 }
